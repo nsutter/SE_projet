4b6ff9fa571e16bee68bfdc0b1f9555324862ddf
nsutter <nsutter@etu.unistra.fr>
2016-04-07 12:52:33 +0200

    [correction] kv_del fusion des cases

--- kv.c
+++ kv.c
@@ -907,6 +907,7 @@ int kv_del(KV * kv, const kv_datum * key)
                       if(write(kv->fd4, &deux, 4) != 4){return -1;}
                       lg_atruncate=lg+lg_atruncate;
                       pos_cle=off;
+                      atruncate=pos_tmp-8;
                       flag_while++;
                       if(lseek(kv->fd4, pos_tmp, SEEK_SET) == -1){return -1;}
                     }
@@ -923,6 +924,7 @@ int kv_del(KV * kv, const kv_datum * key)
                       int deux = 2;
                       if(write(kv->fd4, &deux, 4) != 4){return -1;}
                       lg_atruncate=lg+lg_atruncate;
+                      atruncate=pos_tmp-12;
                       flag_while++;
                       if(lseek(kv->fd4, pos_tmp, SEEK_SET) == -1){return -1;}
                     }
